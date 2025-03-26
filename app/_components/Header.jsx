8683fc16bd005370556d632cf18439adefab0990
Gautam Kumar Mahato <GA20243764@wipro.com>
2025-03-26 17:40:25 +0530

    fixed the path issue

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
                   href="/"
                   className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
-                  Home
+                  Homee
                 </Link>
                 <Link
                   href="#guides"
